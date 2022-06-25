4c0cfca6ef54b986cbf231c806c9aefb5b294d79
Farid Aziz <faridazizw@gmail.com>
2022-06-25 10:27:07 +0700

    first

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+import tkinter
+import numpy
+import cv2
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+main_window = tkinter.Tk()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main_window.mainloop()
